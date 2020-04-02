a5476998a71607344cfb90532a47f4b11d23efbb
黄锦 <jin.huang02@hand-china.com>
2020-04-02 21:02:58 +0800

    feat(ts-axios): 处理请求 url 参数

---
+++ examples/base/app.ts
@@ -0,0 +1,68 @@
+/*
+ * @Description: 
+ * @Date: 2020-04-02 17:34:20
+ * @Author: HJ <jinhuang02@hand-china.com>
+ * @Copyright: Copyright (c) 2018, Hand
+ */
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: 'bar',
+    baz: null
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'bar'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+  params: {
+    bar: 'baz'
+  }
+})
---
+++ helpers/util.ts
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Date: 2020-04-02 17:25:20
+ * @Author: HJ <jinhuang02@hand-china.com>
+ * @Copyright: Copyright (c) 2018, Hand
+ */
+const toString = Object.prototype.toString
+
+export function isDate (val: any): val is Date {
+  return toString.call(val) === '[object Date]'
+}
+
+export function isObject (val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
