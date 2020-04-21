0075e7ef98ba9fa6cb5ecc66eb23b2f4fc886695
黄锦 <jin.huang02@hand-china.com>
2020-04-21 10:32:08 +0800

    feat(ts-axios): 完成 Jest 安装配置，完成 util 模块测试

---
+++ test/boot.ts
@@ -0,0 +1,6 @@
+/*
+ * @Description: 运行 npm test 先执行这里的文件
+ * @Date: 2020-04-21 10:14:34
+ * @Author: HJ <jinhuang02@hand-china.com>
+ * @Copyright: Copyright (c) 2018, Hand
+ */
---
+++ test/helpers/boot.ts
@@ -0,0 +1,6 @@
+/*
+ * @Description: 
+ * @Date: 2020-04-20 17:40:26
+ * @Author: HJ <jinhuang02@hand-china.com>
+ * @Copyright: Copyright (c) 2018, Hand
+ */
